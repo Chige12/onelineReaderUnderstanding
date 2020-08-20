ac16aa6fb667ddf6ea9022bff992b24fb48a34e3
chige12 <tige1215.technic@gmail.com>
2020-08-20 19:53:54 +0900

    toriaezu

--- nuxt.config.js
+++ nuxt.config.js
@@ -98,12 +98,13 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config) {
-      if (process.server && process.browser) {
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/,
           options: {
             fix: true
